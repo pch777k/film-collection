1ece7d08cedc9daab1635329b8d98aa010a4ee0b
Pawel Chmiel <pch777@gazeta.pl>
2020-05-12 18:19:23 +0200

    refactor code

--- src/main/java/pl/film/controller/FilmController.java
+++ src/main/java/pl/film/controller/FilmController.java
@@ -2,7 +2,6 @@ package pl.film.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,8 +18,11 @@ import pl.film.service.FilmService;
 @Controller
 public class FilmController {
 	
-	@Autowired
 	private FilmService filmService;
+
+	public FilmController(FilmService filmService) {
+		this.filmService = filmService;
+	}
 
 	@RequestMapping({"/","/index"})
 	public String viewHomePage(Model model) {
